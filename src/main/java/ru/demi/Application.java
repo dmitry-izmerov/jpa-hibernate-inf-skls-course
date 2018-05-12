9637cc9351acd6c5f2e23c3795c56e06311938e0
demi <idd90i@gmail.com>
2018-05-12 21:44:46 +0300

    5-5 how to map a list of embeddable type elements

--- src/main/java/ru/demi/Application.java
+++ src/main/java/ru/demi/Application.java
@@ -15,29 +15,38 @@ public class Application {
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		session.beginTransaction();
 
-        Address address = new Address();
-        address.setAddressLine1("Moroseyka, 11");
-        address.setAddressLine2("Petrovka, 22");
-        address.setCity("Moscow");
-        address.setZipCode("120204");
+        Address address1 = new Address();
+        address1.setAddressLine1("Moroseyka, 11");
+        address1.setAddressLine2("Petrovka, 22");
+        address1.setCity("Moscow");
+        address1.setState("Moscow");
+        address1.setZipCode("120204");
+
+        Address address2 = new Address();
+        address2.setAddressLine1("line 1");
+        address2.setAddressLine2("line 2");
+        address2.setCity("Tver");
+        address2.setState("Tverskaya oblast");
+        address2.setZipCode("160303");
 
 		// user
-        /*LocalDate birthDate = LocalDate.of(1990, 4, 5);
+        LocalDate birthDate = LocalDate.of(1990, 4, 5);
         User user = new User("Vasya", "Pupkin", birthDate, "vasya@mail.ru", "admin");
-        user.setAddress(address);
-        session.save(user);*/
+        user.getAddresses().add(address1);
+        user.getAddresses().add(address2);
+        session.save(user);
 
         // bank
-        Bank bank = new Bank();
+        /*Bank bank = new Bank();
         bank.setName("VTB");
-        bank.setAddress(address);
+        bank.setAddress(address1);
         bank.setCreatedBy("Vasya Petrov");
         bank.setCreatedDate(new Date());
         bank.setLastUpdatedBy("Vasya Petrov");
         bank.setLastUpdatedDate(new Date());
         bank.getContacts().put("CLIENT_MANAGER", "Timofei");
         bank.getContacts().put("SELLER", "Alex");
-        session.save(bank);
+        session.save(bank);*/
 
         session.getTransaction().commit();
 		session.close();
--- src/main/java/ru/demi/entities/User.java
+++ src/main/java/ru/demi/entities/User.java
@@ -6,15 +6,19 @@ import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.AttributeOverride;
 import javax.persistence.AttributeOverrides;
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
-import javax.persistence.Embedded;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -69,10 +73,11 @@ public class User {
 	@Transient
     private boolean valid;
 
-	@Embedded
+	@ElementCollection
+    @CollectionTable(name = "user_address", joinColumns = @JoinColumn(name = "user_id"))
     @AttributeOverrides({
         @AttributeOverride(name = "addressLine1", column = @Column(name = "user_address_line_1")),
         @AttributeOverride(name = "addressLine2", column = @Column(name = "user_address_line_2"))
     })
-	private Address address;
+	private List<Address> addresses = new ArrayList<>();
 }
